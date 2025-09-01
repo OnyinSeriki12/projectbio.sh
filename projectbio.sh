551ff14c963ac1c353a87d6034ab21acd3dc81ac
OnyinSeriki12 <serahseriki001@gmail.com>
2025-09-01 11:20:19 +0100

    Add files via upload

---
+++ projectbio.sh
@@ -0,0 +1,358 @@
+    1  bash--version
+    2  bash --version
+    3  echo 'Hello Hackbio'
+    4  hackbio=1 
+    5  echo $hackbio
+    6  pwd
+    7  ls
+    8  mkdir hackbio
+    9  pwd
+   10  ls
+   11  rm README-cloudshell.txt
+   12  ls
+   13  mkdir-p hackbio/bash_course moudle_1
+   14  ls
+   15  mkdir-p hackbio/bash_course
+   16  # -Recurse deletes the contents of the folder.
+   17  # -Force will remove read-only or hidden files without prompting.
+   18  Remove-Item -Path "C:\path\to\your_folder" -Recurse -Force
+   19  rmdir /s /q Hackbio
+   20  rmdir Hackbio
+   21  rmdir 'HACKBIO'
+   22  pwd
+   23  mdir serahseriki cv
+   24  ls
+   25  mkdir-hackbio_course
+   26  mkdir-hackbio/bash_course
+   27  pwd
+   28  mkdir cv
+   29  ls
+   30  mkdir folder1 
+   31  mkdir -p research 
+   32  ls
+   33  pwdwd
+   34  ls
+   35  rm Readme-cloudshell.txt
+   36  rm hackbio
+   37  rm mkdir 'hackbio'
+   38  ls
+   39  pwd
+   40  ls
+   41  pwd
+   42  ls
+   43  rm cv folder1
+   44  rm mkdir 'cv''folder1'
+   45  ls
+   46  pwd
+   47  ls
+   48  ls -p
+   49  ls -l
+   50  ls -h
+   51  ls -t
+   52  ls -r
+   53  'cd'
+   54  ls
+   55  mkdir 'hackbio'
+   56  -p hackbio/onyi_course
+   57  -p hackbio/bash_moudle1
+   58  -p hackbio/bash_course/moudle_1
+   59  pwd
+   60  mkdir seriki
+   61  ls
+   62  pwd
+   63  ls
+   64  remove -item -path cv folder1 hackbio
+   65  pwd
+   66  whoami
+   67  ls
+   68  ls -l
+   69  ls -la
+   70  ls -lh
+   71  cd
+   72  cd /tmp
+   73  cd -
+   74  mkdir folder
+   75  mkdir -p mydata/project
+   76  touch myfile.text
+   77  touct 
+   78  touch myfile.txt
+   79  pwd
+   80  ls
+   81  touch myfirstfile.txt
+   82  pwd
+   83  awk
+   84  echo ''Onyinoyi Sarah Seriki''
+   85  echo ''Sarah Onyinoyi seriki''
+   86  mkdir ''Sarah Onyinoyi seriki''
+   87  ls
+   88  mv ''sarah Onyinoyi Seriki'' ''sarah onyinoyi seriki.txt''
+   89  mv 'seriki.txt'
+   90  'mv --help'
+   91  rm -r serahseriki001
+   92  echo 'sarah onyinoyi seriki'
+   93  mkdir 'sarah onyinoyi seriki'
+   94  ls
+   95  mkdir 'biocomputing'
+   96  &&
+   97  '&&'
+   98  mkdir biocomputing && cd biocomputing
+   99  echo ''sarah onyinoyi seriki''
+  100  mkdir 'sarah onyinoyi seriki'
+  101  echo 'sarah onyinoyi seriki'
+  102  mkdir 'sarah onyinoyi seriki'
+  103  ls
+  104  echo 'sarah onyinoyi seriki'
+  105  mkdir 'sarah onyinoyi seriki'
+  106  mkdir 'biocomputing'
+  107  cd biocomputing
+  108  weget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+  109  weget 'https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+  110  '
+  111  weget 'https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna ;https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk ;https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk '
+  112  echo 'sarah onyinoyi seriki'
+  113  mkdir 'sarah onyinoyi seriki'
+  114  ls
+  115  mkdir biocomputing && cd biocomputing
+  116  pwd
+  117  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+  118  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+  119  ls
+  120  mv wildtype.fna "Sarah Onyinoiyi Seriki" 
+  121  ls "Sarah Onyinoiyi Seriki" 
+  122  rm wildtype.gbk.* 
+  123  ls *.gbk	 
+  124  cat wildtype.fna 
+  125  cat: wildtype.fna: No such file or directory
+  126  serahseriki001@cloudshell:~/biocomputing$  
+  127  cat: wildtype.fna: No such file or directory
+  128  serahseriki001@cloudshell:~/biocomputing$   
+  129  /TATATATA 
+  130  pwd
+  131  ls
+  132  ls "Sarah Onyinoiyi Seriki"
+  133  grep "TATATATA" "Sarah Onyinoiyi Seriki/wildtype.fna"
+  134  	
+  135  ls -l 
+  136  mv "Sarah Onyinoiyi Seriki" "Sarah Onyinoiyi Seriki.txt"
+  137  	
+  138  mkdir "Sarah Onyinoiyi Seriki" 
+  139  mv wildtype.fna "Sarah Onyinoiyi Seriki" 
+  140  grep "TATATATA" "Sarah Onyinoiyi Seriki/wildtype.fna"		 
+  141  serahseriki001@cloudshell:~/biocomputing$ ^C 
+  142  ls "Sarah Onyinoiyi Seriki" 
+  143  ls
+  144  grep "TATATATA" "Sarah Onyinoiyi Seriki/wildtype.gbk" 
+  145  ls
+  146  grep "TATATATA" "Sarah Onyinoiyi Seriki/wildtype.fna" 
+  147  find . -name "wildtype.fna" 
+  148  mv biocomputing/wildtype.fna "Sarah Onyinoiyi Seriki" 
+  149  bash--version
+  150  echo "Sarah Onyinoiyi Seriki" 
+  151  mkdir "Sarah Onyinoiyi Seriki" 
+  152  ls
+  153  mkdir biocomputing && cd biocomputing 
+  154  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+  155  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+  156  ls
+  157  mv wildtype.fna "../Sarah Onyinoiyi Seriki" 
+  158  rm wildtype.gbk.1 
+  159  grep "TATATATA" "../Sarah Onyinoiyi Seriki/wildtype.fna"
+  160  grep "TATA" "../Sarah Onyinoiyi Seriki/wildtype.fna"
+  161  grep -v "^>" wildtype.gbk | wc -l 
+  162  grep "^LOCUS" wildtype.gbk
+  163  grep "SOURCE" wildtype.gbk 
+  164  grep "/gene=" wildtype.gbk 
+  165  clear 
+  166  echo "Sarah Onyinoiyi Seriki" 
+  167  mkdir "Sarah Onyinoiyi Seriki" 
+  168  mkdir biocomputing && cd biocomputing
+  169  	
+  170  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+  171  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+  172  mv wildtype.fna "../Sarah Onyinoiyi Seriki" 
+  173  grep "TATATATA" "../Sarah Onyinoiyi Seriki/wildtype.fna"
+  174  grep "TATA" "../Sarah Onyinoiyi Seriki/wildtype.fna"
+  175  grep "TATATATA" "../Sarah Onyinoiyi Seriki/wildtype.fna" > mutant_matches.txt 
+  176  grep -v "^>" wildtype.gbk | wc -l 
+  177  grep "^LOCUS" wildtype.gbk 
+  178  grep "SOURCE" wildtype.gbk	 
+  179  grep "/gene=" wildtype.gbk 
+  180  clear 
+  181  history 
+  182  ls "Sarah Onyinoiyi Seriki"
+  183  ls biocomputing
+  184  ls "Sarah Onyinoiyi Seriki" 
+  185  ls biocomputingls biocomputing 
+  186  echo "Sarah Onyinoiyi Seriki"
+  187  mkdir -p "Sarah Onyinoiyi Seriki"
+  188  mkdir -p biocomputing && cd biocomputing
+  189  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+  190  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+  191  mv wildtype.fna "../Sarah Onyinoiyi Seriki"	
+  192  rm -f wildtype.gbk.1	
+  193  echo "Checking if .fna is mutant or wild type..."	
+  194  grep "TATATATA" "../Sarah Onyinoiyi Seriki/wildtype.fna"
+  195  grep "TATA" "../Sarah Onyinoiyi Seriki/wildtype.fna"
+  196  grep "TATATATA" "../Sarah Onyinoiyi Seriki/wildtype.fna" > mutant_matches.txt
+  197  echo "Line count in .gbk (excluding header):"
+  198  grep -v "^>" wildtype.gbk | wc -l
+  199  echo "Sequence length (LOCUS line):"
+  200  grep "^LOCUS" wildtype.gbk
+  201  echo "Source organism:"
+  202  grep "SOURCE" wildtype.gbk
+  203  echo "Gene names:"	
+  204  grep "/gene=" wildtype.gbk
+  205  history
+  206  echo "Files in Sarah Onyinoiyi Seriki folder:"
+  207  ls "../Sarah Onyinoiyi Seriki"
+  208  echo "Files in biocomputing folder:"
+  209  ls .
+  210  echo "Files in Sarah Onyinoiyi Seriki folder:"
+  211  pwd
+  212  ls
+  213  echo 'sarah onyinoyi seriki'
+  214  mkdir -p 'sarah onyinoyi seriki'
+  215  ls
+  216  mkdir -p biocomputing && cd biocomputing
+  217  echo ">>> Downloading files..."
+  218  wget -q https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+  219  wget -q https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+  220  mv -f wildtype.fna "../Sarah Onyinoiyi Seriki" 
+  221  rm -f wildtype.gbk.1 wildtype.gbk.2 
+  222  echo "Sarah Onyinoyi Seriki"
+  223  mkdir "Sarah Onyinoyi Seriki" 
+  224  ls
+  225  cd "Sarah Onyinoyi Seriki" 
+  226  mkdir biocomputing && cd biocomputing 
+  227  mkdir biocomputing && cd biocomputing && pwd 
+  228  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+  229  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+  230  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk 
+  231  mv wildtype.fna "Sarah Onyinoyi Seriki/" 
+  232  cd ~ 
+  233  pwd 
+  234  cd biocomputing 
+  235  ls -la 
+  236  cd ~
+  237  rm -rf "Sarah Onyinoyi Seriki" biocomputing 
+  238  echo "Sarah Onyinoyi Seriki" 
+  239  mkdir "Sarah Onyinoyi Seriki"
+  240  ls
+  241  rm -rf 'Sarah Onyinoiyi Seriki' 'sarah onyinoyi seriki' 'Sarah Onyinoyi Seriki' 
+  242  mkdir "Sarah Onyinoyi Seriki"
+  243  ls -la 
+  244  ls
+  245  cd ~
+  246  rm README-cloudshell.txt 
+  247  rm -rf "Sarah Onyinoyi Seriki" 
+  248  echo "Sarah Onyinoyi Seriki" 
+  249  mkdir "Sarah Onyinoyi Seriki" 
+  250  ls
+  251  mkdir biocomputing && cd biocomputing 
+  252  pwd
+  253  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+  254  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+  255  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk 
+  256  mv wildtype.fna "../Sarah Onyinoyi Seriki/" 
+  257  rm wildtype.gbk.1 
+  258  grep -i "tatatata\|tata" "../Sarah Onyinoyi Seriki/wildtype.fna" 
+  259  if grep -qi "tatatata" "../Sarah Onyinoyi Seriki/wildtype.fna"; then     echo "Mutant sequence found! Saving matching lines...";     grep -i "tatatata" "../Sarah Onyinoyi Seriki/wildtype.fna" > mutant_lines.txt;     echo "Matching lines saved to mutant_lines.txt"; else     echo "No mutant sequence (tatatata) found in the file."; fi 
+  260  tail -n +2 wildtype.gbk | wc -l 
+  261  head -n 1 wildtype.gbk | awk '{print "Sequence length:", $3, "bp"}' 
+  262  grep "^SOURCE" wildtype.gbk | sed 's/SOURCE *//' | head -n 1 
+  263  grep '/gene=' wildtype.gbk 
+  264  clear
+  265  echo "=== Commands used today ==="
+  266  echo "1. Print name:"
+  267  echo "   echo \"Sarah Onyinoyi Seriki\""
+  268  echo ""
+  269  echo "2. Create folder with your name:"
+  270  echo "   mkdir \"Sarah Onyinoyi Seriki\""
+  271  echo ""
+  272  echo "3. Create biocomputing directory and change to it:"
+  273  echo "   mkdir biocomputing && cd biocomputing"
+  274  echo ""
+  275  echo "4. Download 3 files:"
+  276  echo "   wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna"
+  277  echo "   wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk"
+  278  echo ""
+  279  echo "5. Move .fna file to name folder:"
+  280  echo "   mv wildtype.fna \"../Sarah Onyinoyi Seriki/\""
+  281  echo ""
+  282  echo "6. Delete duplicate .gbk file:"
+  283  echo "   rm wildtype.gbk.1"
+  284  echo ""
+  285  echo "7. Count lines in .gbk file (excluding header):"
+  286  echo "   tail -n +2 wildtype.gbk | wc -l"
+  287  echo ""
+  288  echo "8. Print sequence length from LOCUS tag:"
+  289  echo "   head -n 1 wildtype.gbk | awk '{print \"Sequence length:\", \$3, \"bp\"}'"
+  290  echo ""
+  291  echo "9. Print source organism:"
+  292  echo "   grep \"^SOURCE\" wildtype.gbk | sed 's/SOURCE *//' | head -n 1"
+  293  echo ""
+  294  echo "10. List all gene names:"
+  295  echo "    grep '/gene=' wildtype.gbk" 
+  296  echo "=== Files in 'Sarah Onyinoyi Seriki' folder ==="
+  297  ls -la "../Sarah Onyinoyi Seriki/"
+  298  echo ""
+  299  echo "=== Files in 'biocomputing' folder ==="
+  300  ls -la 
+  301  #this is the end of project 1
+  302  'gcloud projects lists'
+  303  pwd
+  304  ls
+  305  history
+  306  conda activate base
+  307  source activate base 
+  308  pwd
+  309  conda activate base 
+  310  ls ~/anaconda3/bin/conda 
+  311  Activate Base Conda Environment 
+  312  conda activate base 
+  313  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh 
+  314  bash Miniconda3-latest-Linux-x86_64.sh
+  315  bash Miniconda3-latest-Linux-x86_64.sh 
+  316  history
+  317  pwd
+  318  bash Miniconda3-latest-Linux-x86_64.sh 
+  319  source ~/.bashrc 
+  320  conda create --name funtools 
+  321  conda activate funtools 
+  322  conda install -c conda-forge figlet 
+  323  history
+  324  figlet "Serah onyinoyi seriki''
+  325  figlet "Serah Seriki" 
+  326  figlet ''sarah onyinoyi seriki''
+  327  conda activate funtools
+  328  figlet "sarah onyinoyi seriki" 
+  329  conda install -c conda-forge figlet
+  330  figlet "sarah onyinoyi seriki" 
+  331  sudo apt-get update 
+  332  sudo apt-get install figlet 
+  333  figlet "sarah onyinoyi seriki" 
+  334  conda config --add channels bioconda 
+  335  conda config --add channels conda-forge
+  336  bwa 
+  337  conda install -c bioconda bwa 
+  338  conda install -c bioconda blast 
+  339  blastn -help 
+  340  conda install -c bioconda samtools 
+  341  samtools --version 
+  342  conda install -c bioconda spades 
+  343  spades.py --version 
+  344  conda install -c bioconda bcftools 
+  345  bcftools --version 
+  346  conda install -c bioconda fastp
+  347  fastp --version
+  348  History
+  349  history
+  350  Install MultiQC via Bioconda 
+  351  conda install -c bioconda multiqc   
+  352  multiqc --version 
+  353  #the end of project2
+  354  cd /path/to/your/project 
+  355  history
+  356  touch project 1&2 .txt
+  357  history
+  358  history > projectbio.sh
